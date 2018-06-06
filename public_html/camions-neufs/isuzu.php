771a2bcd3615b234f0612b483447106b120d9867
Philippe Tourigny <philtourigny@gmail.com>
2018-06-05 21:37:17 -0400

    Ajout texte site prod

--- public_html/camions-neufs/isuzu.php
+++ public_html/camions-neufs/isuzu.php
@@ -4,8 +4,33 @@
     <div class="wrap">
         <div class="content">
             <div class="shrink">
-                <div class="titrepage">
-                    <h1><?php echo $NOMPAGE; ?></h1>
+                <div class="titrePage">
+                    <h1>Camions neufs Isuzu</h1>
+                </div>
+                <div class="contenu contenu2">
+                    <div>
+                        Camions les plus vendus en Amérique du Nord depuis 1986, les camions Isuzu offrent une performance de haut niveau et des prix surprenants. Présents à travers le Québec, les concessionnaires de Réseau Dynamique sont dépositaires de la gamme complète de camions Isuzu : camion léger (série N), camion moyen (série F), camion lourd (série C&amp;E). Économiques, propres et respectueux de l'environnement, ils s'imposeront comme un atout majeur dans votre entreprise.
+                    </div>
+                    <div class="toggle">
+                        <div class="btnToggle">
+                            <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="javascript:void(0);" linkcollapsegroup="" targetid="" class="open">Informations supplémentaires</a>
+                        </div>
+                        <div class="" style="display: none;">
+                            <span>
+                                <h2>Performance économique</h2>
+                                <p>Reconnus pour leur puissance, leur fiabilité et leur durabilité, les camions Isuzu augmenteront votre productivité et le rendement de votre entreprise. Plus encore, ils changeront votre manière de faire des affaires. Grâce à 80 ans de recherche et développement, la marque peut aujourd'hui se targuer d'avoir conçu des moteurs au diesel performants des plus économiques et à faibles émissions.</p>
+                                <h2>Maniabilité, robustesse et fiabilité</h2>
+                                <p>Isuzu offre une grande variété de modèles correspondant à une tout aussi grande diversité d'usages et de domaines d'activité. Ainsi, ils seront performants tant dans les domaines du déménagement, de la construction, de l'antiparasitaire, du remorquage et du service alimentaire. Leur robustesse et leur fiabilité en feront votre meilleur partenaire d'affaires. Construits avec des technologies de pointe, des matériaux résistants de première qualité et une mécanique d'avant-garde, ils s'avèrent faciles d'entretien et requièrent peu de soins mécaniques.</p>
+                                <h2>Confort à toute épreuve</h2>
+                                <p>Isuzu a toujours prêté une attention particulière au confort dans la conception de ses véhicules. Que la route vous mène en terrain montagneux ou en milieu urbain, tous les modèles Isuzu vous offrent une maniabilité, une stabilité et une sécurité à toute épreuve.</p>
+                                <p>Enfin, avec 80 % de la flotte toujours effective sur la route depuis 1984, les camions Isuzu représentent un investissement puisqu'ils conservent une valeur de revente élevée sur le marché des camions d'occasion.</p>
+                                <p>Faites confiance aux camions Isuzu et à la force du Réseau Dynamique et optez pour un véhicule fiable et économique.</p>
+                                <p>
+                                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="http://www.isuzutruck.ca/" target="_blank">Visitez le site officiel d'Isuzu</a>
+                                </p>
+                            </span>
+                        </div>
+                    </div>
                 </div>
                 <div class="contenu">
                     <div class="contenu2">
@@ -19,8 +44,7 @@
             </div>
         </div>
     </div>
-    <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/footer/_footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/footer/_footer.php'); ?>    
     </form>
-    
 </body>
 </html>
