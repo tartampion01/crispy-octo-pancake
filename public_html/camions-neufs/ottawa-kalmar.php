ab2654c27bfc1515ba38155bc64696e775d0bac1
Philippe Tourigny <philtourigny@gmail.com>
2018-06-05 21:37:22 -0400

    Ajout texte site prod

--- public_html/camions-neufs/ottawa-kalmar.php
+++ public_html/camions-neufs/ottawa-kalmar.php
@@ -4,16 +4,32 @@
     <div class="wrap">
         <div class="content">
             <div class="shrink">
-                <div class="titrepage">
-                    <h1><?php echo $NOMPAGE; ?></h1>
+                <div class="titrePage">
+                    <h1>Camions neufs Ottawa Kalmar</h1>
                 </div>
-                <div class="contenu">
-                    <div class="contenu2">
-                        <p></p>
-                        <h2></h2>
-                        <p></p>
-                        <p></p>
-                        <p></p>
+                <div class="contenu contenu2">
+                    <div>
+                        Réseau Dynamique, dépositaire Ottawa Kalmar, offre une large sélection de tracteurs de terminal aux différents types d'industries et d'usages.
+                    </div>
+                    <div class="toggle" itemid="">
+                        <div class="btnToggle" itemid="">
+                            <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="javascript:void(0);" linkcollapsegroup="" targetid="" class="open">Informations supplémentaires</a>
+                        </div>
+                        <div class="" style="display: none;">
+                            <span>
+                                <h2>Le meilleur tracteur de terminal pour vos opérations</h2>
+                                <p>Ottawa Kalmar est chef de file dans la construction de tracteurs de terminal pour une bonne raison : aucun autre camion n'égale les propriétés ergonomiques, la fiabilité inébranlable et le soutien offert par Ottawa auprès de sa clientèle. Au sommet depuis plus de 50 ans, les camions Ottawa Kalmar continuent avec succès à déplacer les remorques autour des centres de distribution, des stations de transfert, des terminaux de camions ainsi que des ports et autres lieux d'entreposage de conteneurs.</p>
+                                <p>En hiver et en été, de jour comme de nuit, vous avez besoin d'un tracteur de terminal polyvalent et précis au coût d'opération faible. Capables d'opérer dans tous les types de cours, les tracteurs de terminaux Ottawa déplacent des remorques de toutes les tailles, quels que soient leurs paramètres de pivot d'attelage et leur hauteur.</p>
+                                <h2>L'ergonomie au service de la productivité</h2>
+                                <p>Les tracteurs terminaux d'Ottawa Kalmar n'ont pas été construits pour leur beauté. Ils ont été pensés et bâtis pour travailler. En d'autres termes, un aspect du design sera privilégié seulement s'il rend le véhicule plus efficace, plus rapide et plus productif. L'innovation en continu des produits est possible grâce à un travail de recherche sans relâche avec l'appui de près de 50 concessionnaires en Amérique du Nord.</p>
+                                <p>Testés et retestés, les camions Ottawa Kalmar sont conçus de sorte à offrir une performance ergonomique supérieure pour le conducteur. Ainsi, l'entière conception de la cabine spacieuse, commode et exceptionnellement confortable améliore grandement la productivité de l'opérateur. La grande surface vitrée offre une excellente visibilité permettant à l'opérateur de manœuvrer avec une vue imprenable sur tous les angles à l'avant, à l'arrière et sur les côtés.</p>
+                                <p>Avec la qualité éprouvée des véhicules Ottawa Kalmar et la force du Réseau Dynamique vous optez pour un véhicule fiable et un service professionnel et dévoué.</p>
+                                <p>Pour toutes informations au sujet des camions Kalmar Ottawa, veuillez communiquer avec Steeve Brousseau au 418 580-9049.</p>
+                                <p>
+                                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="http://www.ottawatrucksna.com" target="_blank">Visitez le site officiel d'Ottawa Kalmar</a>
+                                </p>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -21,6 +37,5 @@
     </div>
     <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/footer/_footer.php'); ?>
     </form>
-    
 </body>
 </html>
