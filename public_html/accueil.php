6848ce82ae405ae1319c6927c868c79f90cec53c
Philippe Tourigny <philtourigny@gmail.com>
2018-06-08 00:48:18 -0400

    trim de la page et update des liens

--- public_html/accueil.php
+++ public_html/accueil.php
@@ -14,12 +14,12 @@
                                     <img name="image" title="" src="_assets/images/homeBanner/retour-lonestar.png" alt="Retour Lonestar">
                                 </div>
                                 <div>
-                                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="/camions-neufs/isuzu">
+                                    <a name="hyperlien" href="/camions-neufs/isuzu">
                                         <img name="image" title="" src="_assets/images/homeBanner/isuzu.jpg" alt="isuzu">
                                     </a>
                                 </div>
                                 <div>
-                                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="/remorques-neuves">
+                                    <a name="hyperlien" href="/remorques-neuves">
                                         <img name="image" title="" src="_assets/images/homeBanner/doepker.jpg" alt="doepker">
                                     </a>
                                 </div>
@@ -31,181 +31,119 @@
                     </div>
             </div>
 	</div>
-	<div class="content accueil" data-staticClassNames="content accueil">
-            <div class="shrink" data-staticClassNames="shrink">
-                <div class="vehiculeDisponible" data-staticClassNames="vehiculeDisponible">
+	<div class="content accueil">
+            <div class="shrink">
+                <div class="vehiculeDisponible">
                     <h2>V&#233;hicules disponibles</h2>
                 </div>
                     <!--<img name="image" title="" src="_assets/images/menu_images/promo-site-web.jpg" alt="promo site web" />-->
-                <div class="Nad579b8ffd42482c906005c978ef9afa_N57722c9eed144b248e2f78f30b1246ed cat" data-staticClassNames="Nad579b8ffd42482c906005c978ef9afa_N57722c9eed144b248e2f78f30b1246ed cat">
-                    <span data-staticClassNames="">
-                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="camions-neufs/camions-lourds-neufs-international.html" target="_self">
-                    <span class="image" data-staticClassNames="image">
-                    <img name="image" title="" src="_assets/images/menu_images/cat-camion-international.jpg" alt="Camion International" /></span>
-                    <span class="imageMobile" data-staticClassNames="imageMobile">
-                    <img name="image" title="" src="_assets/images/menu_images/ban-camion-international.jpg" alt="Camion International" /></span>
-                    <span class="orange" data-staticClassNames="orange">
-                    <span class="titre" data-staticClassNames="titre">Camion international</span>
-                    <span class="voir" data-staticClassNames="voir">Voir</span></span></a></span>
+                <div class="cat">
+                    <span>
+                        <a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::CamionsNeufs,page::CamionsInternational)?>" target="_self">
+                            <span class="image">
+                                <img name="image" title="" src="_assets/images/menu_images/cat-camion-international.jpg" alt="Camion International" />
+                            </span>
+                            <span class="imageMobile">
+                                <img name="image" title="" src="_assets/images/menu_images/ban-camion-international.jpg" alt="Camion International" />
+                            </span>
+                            <span class="orange">
+                                <span class="titre">Camion international</span>
+                                <span class="voir">Voir</span>
+                            </span>
+                        </a>
+                    </span>
                 </div>
-                    <div class="cpy1495744198806_Nad579b8ffd42482c906005c978ef9afa_N57722c9eed144b248e2f78f30b1246ed cat" data-staticClassNames="cpy1495744198806_Nad579b8ffd42482c906005c978ef9afa_N57722c9eed144b248e2f78f30b1246ed cat">
-                    <span data-staticClassNames="">
-                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="camions-neufs/ottawa-kalmar.html" target="_self">
-                    <span class="image" data-staticClassNames="image">
-                    <img name="image" title="" src="_assets/images/menu_images/cat-tracteur-terminal-ottawa-kalmar.jpg" alt="Tracteur Terminal Ottawa Kalmar" /></span>
-                    <span class="imageMobile" data-staticClassNames="imageMobile">
-                    <img name="image" title="" src="_assets/images/menu_images/ban-tracteur-terminal.jpg" alt="Tracteur Terminal" /></span>
-                    <span class="orange" data-staticClassNames="orange">
-                    <span class="titre" data-staticClassNames="titre">Tracteur de Terminal Ottawa Kalmar</span>
-                    <span class="voir" data-staticClassNames="voir">Voir</span></span></a></span>
+                <div class="cat">
+                    <span>
+                        <a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::CamionsNeufs,page::CamionsOttawaKalmar)?>" target="_self">
+                            <span class="image">
+                                <img name="image" title="" src="_assets/images/menu_images/cat-tracteur-terminal-ottawa-kalmar.jpg" alt="Tracteur Terminal Ottawa Kalmar" />
+                            </span>
+                            <span class="imageMobile">
+                                <img name="image" title="" src="_assets/images/menu_images/ban-tracteur-terminal.jpg" alt="Tracteur Terminal" />
+                            </span>
+                            <span class="orange">
+                                <span class="titre">Tracteur de Terminal Ottawa Kalmar</span>
+                                <span class="voir">Voir</span>
+                            </span>
+                        </a>
+                    </span>
                 </div>
-                    <div class="Nf70001948dcc4a3cbf1caa12ad2a3fd0_N57722c9eed144b248e2f78f30b1246ed cat" data-staticClassNames="Nf70001948dcc4a3cbf1caa12ad2a3fd0_N57722c9eed144b248e2f78f30b1246ed cat">
-                    <span data-staticClassNames=""><a name="hyperlien" onclick="javascript:RegisterClick(this);" href="camions-neufs/isuzu.html" target="_self">
-                    <span class="image" data-staticClassNames="image">
-                    <img name="image" title="" src="_assets/images/menu_images/cat-camion-isuzu.jpg" alt="Camion Isuzu" /></span>
-                    <span class="imageMobile" data-staticClassNames="imageMobile">
-                    <img name="image" title="" src="_assets/images/menu_images/ban-camion-isuzu.jpg" alt="Camion Isuzu" /></span>
-                    <span class="orange" data-staticClassNames="orange">
-                    <span class="titre" data-staticClassNames="titre">Camion Isuzu</span>
-                    <span class="voir" data-staticClassNames="voir">Voir</span></span></a></span>
+                <div class="cat">
+                    <span>
+                        <a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::CamionsNeufs,page::CamionsIsuzu)?>" target="_self">
+                            <span class="image">
+                                <img name="image" title="" src="_assets/images/menu_images/cat-camion-isuzu.jpg" alt="Camion Isuzu" />
+                            </span>
+                            <span class="imageMobile">
+                                <img name="image" title="" src="_assets/images/menu_images/ban-camion-isuzu.jpg" alt="Camion Isuzu" />
+                            </span>
+                            <span class="orange">
+                                <span class="titre">Camion Isuzu</span>
+                                <span class="voir">Voir</span>
+                            </span>
+                        </a>
+                    </span>
                 </div>
-                <div class="Nd3049420ea814009a80de3cc26b4512d_N57722c9eed144b248e2f78f30b1246ed cat" data-staticClassNames="Nd3049420ea814009a80de3cc26b4512d_N57722c9eed144b248e2f78f30b1246ed cat">
-                    <span data-staticClassNames=""><a name="hyperlien" onclick="javascript:RegisterClick(this);" href="remorques-neuves.html" target="_self">
-                    <span class="image" data-staticClassNames="image">
-                    <img name="image" title="" src="_assets/images/menu_images/cat-remorque-doepkler.jpg" alt="Remorque Doepkler" /></span>
-                    <span class="imageMobile" data-staticClassNames="imageMobile">
-                    <img name="image" title="" src="_assets/images/menu_images/ban-remorques-doepker.jpg" alt="Remorques Doepker" /></span>
-                    <span class="orange" data-staticClassNames="orange">
-                    <span class="titre" data-staticClassNames="titre">Remorque Doepker</span>
-                    <span class="voir" data-staticClassNames="voir">Voir</span></span></a></span>
+                <div class="cat">
+                    <span>
+                        <a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::RemorquesNeuves,page::RemorquesDoepker)?>" target="_self">
+                            <span class="image">
+                                <img name="image" title="" src="_assets/images/menu_images/cat-remorque-doepkler.jpg" alt="Remorque Doepkler" />
+                            </span>
+                            <span class="imageMobile">
+                                <img name="image" title="" src="_assets/images/menu_images/ban-remorques-doepker.jpg" alt="Remorques Doepker" />
+                            </span>
+                            <span class="orange">
+                                <span class="titre">Remorque Doepker</span>
+                                <span class="voir">Voir</span>
+                            </span>
+                        </a>
+                    </span>
                 </div>
-                <div class="Nfaa66173e84d4032a0c7bf476a00e692_N57722c9eed144b248e2f78f30b1246ed cat" data-staticClassNames="Nfaa66173e84d4032a0c7bf476a00e692_N57722c9eed144b248e2f78f30b1246ed cat">
-                    <span data-staticClassNames=""><a name="hyperlien" onclick="javascript:RegisterClick(this);" href="vehicules-utilitaires/mini-excavatrices.html" target="_self">
-                    <span class="image" data-staticClassNames="image">
-                    <img name="image" title="" src="_assets/images/menu_images/cat-yanmar.jpg" alt="Yanmar" /></span>
-                    <span class="imageMobile" data-staticClassNames="imageMobile">
-                    <img name="image" title="" src="_assets/images/menu_images/ban-yanmar.jpg" alt="Yanmar" /></span>
-                    <span class="orange" data-staticClassNames="orange">
-                    <span class="titre" data-staticClassNames="titre">Yanmar</span>
-                    <span class="voir" data-staticClassNames="voir">Voir</span></span></a></span>
+                <div class="cat">
+                    <span>
+                        <a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::VehiculesUtilitaires,page::VehiculesUtilitairesMiniExcavatrices)?>" target="_self">
+                            <span class="image">
+                                <img name="image" title="" src="_assets/images/menu_images/cat-yanmar.jpg" alt="Yanmar" />
+                            </span>
+                            <span class="imageMobile">
+                                <img name="image" title="" src="_assets/images/menu_images/ban-yanmar.jpg" alt="Yanmar" />
+                            </span>
+                            <span class="orange">
+                                <span class="titre">Yanmar</span>
+                                <span class="voir">Voir</span>
+                            </span>
+                        </a>
+                    </span>
                 </div>
             </div>
 	</div>
-        <div class="Nms1408366976453 services" data-staticClassNames="Nms1408366976453 services">
-            <div class="Nms1408366976454 shrink" data-staticClassNames="Nms1408366976454 shrink">
-                <div class="N5d026eb59a5b45d9a4c2f9654f313e29_N5775d976d3c34098a628a95438a2fde7 item succursales" data-staticClassNames="N5d026eb59a5b45d9a4c2f9654f313e29_N5775d976d3c34098a628a95438a2fde7 item succursales">
-                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="nous-joindre.html" target="_self">
-                    <span class="titre" data-staticClassNames="titre">Localisez<br/>
+        <div class="services">
+            <div class="shrink">
+                <div class="item succursales">
+                    <a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::Root,page::NousJoindre) ?>" target="_self">
+                    <span class="titre">Localisez<br/>
                     <strong>nos succursales</strong></span>
                     <img name="image" title="" src="_assets/images/menu_images/accueil-succursales-mobile.jpg" alt="Localisez nos succursales" /></a>
                 </div>
-                <div class="Na8fd7aa61aff4421996d0c8f1ede87cd_N5775d976d3c34098a628a95438a2fde7 item pieces" data-staticClassNames="Na8fd7aa61aff4421996d0c8f1ede87cd_N5775d976d3c34098a628a95438a2fde7 item pieces">
-                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="pieces-services/pieces-accessoires.html" target="_self">
-                    <span class="titre" data-staticClassNames="titre">Profitez de notre<br/>
+                <div class="item pieces">
+                    <a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::PiecesService,page::PiecesEtServicesPiecesEtAsccessoires) ?>" target="_self">
+                    <span class="titre">Profitez de notre<br/>
                     <strong>Service de pi&#232;ces</strong></span>
                     <img name="image" title="" src="_assets/images/menu_images/accueil-pieces-mobile.jpg" alt="Profitez de notre Service de pièces" /></a>
                 </div>
-                <div class="Nc5a1a7c73d8d4932b2ebda65c61d886f_N5775d976d3c34098a628a95438a2fde7 item urgence" data-staticClassNames="Nc5a1a7c73d8d4932b2ebda65c61d886f_N5775d976d3c34098a628a95438a2fde7 item urgence">
-                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="urgence-routiere-24h.html" target="_self">
-                    <span class="titre" data-staticClassNames="titre">Des ennuis?<br/>
+                <div class="item urgence">
+                    <a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::Root,page::UrgenceRoutiere24H) ?>" target="_self">
+                    <span class="titre">Des ennuis?<br/>
                     <strong >Urgence routi&#232;re 24h</strong></span>
                     <img name="image" title="" src="_assets/images/menu_images/accueil-urgence-mobile.jpg" alt="Des ennuis? Urgence routière 24h" /></a>
                 </div>
             </div>
 	</div>
-        <div id="c5_c68" class="Nms1408366976455 push" data-staticClassNames="Nms1408366976455 push"></div>
+        <div id="c5_c68" class="push"></div>
     </div>
-<script type="text/javascript">    //<![CDATA[
-$(document).ready(function () {
-
-  $("body").attr("ontouchstart", "");
-  $("body").attr("onmouseover", "");
-
-  var $menuMobile = $(".menuMobile");
-  if ($(window).width() < 641) {
-    $menuMobile.css("left", -$menuMobile.outerWidth());
-  } else {
-    $menuMobile.hide();
-  }
-
-  $(".icoMenuMobile").click(function () {
-    $(this).toggleClass("open");
-
-    $menuMobile.animate({
-      left: parseInt($menuMobile.css('left'), 10) == 0 ?
-          -$menuMobile.outerWidth() :
-          0
-    });
-    $(".wrap").animate({
-      left: parseInt($menuMobile.css('left'), 10) == 0 ?
-        0 :
-        +$menuMobile.outerWidth()
-    });
-    $(".pied").animate({
-      left: parseInt($menuMobile.css('left'), 10) == 0 ?
-        0 :
-        +$menuMobile.outerWidth()
-    });
-  });
-
-  $(".menuCss5").find(".parent > a").click(function () {
-    var open = $(this).parent();
-    if (open.hasClass("expanded")) {
-      open.removeClass("expanded");
-      open.children("div").slideUp();
-    } else {
-      open.addClass("expanded");
-      open.children("div").slideDown("fast", function () {
-        $('html, body').animate({
-          scrollTop: open.offset().top
-        }, 1000);
-      });
-    }
-  });
-  //Faire afficher les filtres
-  $(".toggleFilters").bind("click", openFilters);
-  $(".viewResults a").bind("click", closeFilters);
-  function openFilters() {
-    $(".GpcMenuWrapper").slideDown();
-    $(".toggleFilters").addClass("selected").unbind("click").find(".openClose").bind("click", closeFilters);
-  }
-  function closeFilters() {
-    $(".GpcMenuWrapper").slideUp();
-    $(".toggleFilters").removeClass("selected");
-    setTimeout(function () {
-      $(".toggleFilters").bind("click", openFilters).find(".openClose").unbind("click")
-    }, 250)
-  }
-});
-
-var magicTimeout = "";
-
-$(window).resize(function () {
-  if (magicTimeout != null) {
-    clearTimeout(magicTimeout);
-    magicTimeout = null;
-  }
-  magicTimeout = setTimeout(function () {
-    var $menuMobile = $(".menuMobile");
-    if ($(window).width() < 641) {
-      $menuMobile.show();
-      if (parseInt($menuMobile.css("left")) < 0) {
-        $menuMobile.css("left", -$menuMobile.outerWidth())
-      } else {
-        if ($menuMobile.is(":visible")) {
-          $(".wrap").css("left", $menuMobile.outerWidth());
-          $(".pied").css("left", $menuMobile.outerWidth());
-        }
-      }
-    } else {
-      $menuMobile.hide();
-    }
-  }, 100);
-});
-    //]]>  </script><img alt="" src="#" style="position:absolute;left:-10px;top:-10px;width:1px;height:1px;" />
-
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/footer/_footer.php'); ?>
 </form>
-
 </body>
 </html>
