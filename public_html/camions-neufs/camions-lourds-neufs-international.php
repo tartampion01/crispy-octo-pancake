b3810fe52675d96ad18ef57552defd72aebfb285
tartampion01 <philtourigny@gmail.com>
2018-06-13 15:39:15 -0400

    lien dans classe

--- public_html/camions-neufs/camions-lourds-neufs-international.php
+++ public_html/camions-neufs/camions-lourds-neufs-international.php
@@ -15,7 +15,7 @@
                         <div class="btnToggle">
                             <a name="hyperlien" onclick="toggleInformationSupplementaire('divToggle');" class="information-supplementaire" href="javascript:void(0);">Informations supplémentaires</a>
                         </div>
-                        <div id="divToggle" class="showedZone" data-staticclassnames="showedZone" style="display:none;">
+                        <div id="divToggle" class="showedZone" style="display:none;">
                             <span>
                                 <h2>La puissance de l'innovation</h2>
                                 <p>Que vous soyez sur la route pour la semaine ou pour la fin de semaine, vous avez besoin d'un camion qui vous permettra d'atteindre vos objectifs d'affaires. Élaborés selon les plus hautes normes de qualité obtenues par une recherche constante de l'innovation, les camions International possèdent style et puissance. Avec la fiabilité et la productivité offertes par ces véhicules vous serez en mesure de faire face à tous les défis rencontrés sur la route. International vous aide à respecter vos engagements envers vos clients et à conserver leur confiance.</p>
@@ -23,8 +23,8 @@
                                 <h2>Prenez le volant de vos "affaires"</h2>
                                 <p>Nul ne passe autant de temps que vous sur la route. Les roues, la mécanique et la structure des camions International ont été soumises à des tests qui se sont étendus sur des millions de kilomètres. Soyez assurés que votre véhicule n'expérimentera aucune nouvelle expérience entre vos mains qui n'ait pas été déjà testée par les ingénieurs d’International. En tout temps, vous pourrez compter sur votre camion à 100 % pour vous mener, vous et votre charge, à bon port.</p>
                                 <p>Avec la qualité éprouvée des camions International et la force du Réseau Dynamique vous optez pour un véhicule fiable et un service professionnel et dévoué.</p>
-                                <p>
-                                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="http://ca.internationaltrucks.com/" target="_blank">Visitez le site officiel de International Trucks</a>
+                                <p>"
+                                    <a name="hyperlien" onclick="javascript:RegisterClick(this);" href="<?php echo RD_PageLink::getHref(folder::EXTERNAL,page::EXTERNAL_INTERNATIONAL)?>" target="_blank">Visitez le site officiel de International Trucks</a>
                                 </p>
                             </span>
                         </div>
