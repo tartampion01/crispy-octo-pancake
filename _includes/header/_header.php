8a6259e0f98db1595c914687f8eff3c5fecfcac4
Philippe Tourigny <philtourigny@gmail.com>
2018-06-14 11:42:41 -0400

    enlever sous repertoire inutile

--- _includes/header/_header.php
+++ _includes/header/_header.php
@@ -30,7 +30,7 @@
     <script type="text/javascript">var ShortCultureID = 'fr'; var LongCultureID = 'fr-CA';</script>
     <script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','www.google-analytics.com/analytics.js','ga');ga('create', 'UA-3192388-1');ga('require','displayfeatures');ga('send', 'pageview');</script>
 
-    <link rel="stylesheet" type="text/css" href="../../_assets/css/default/defaultGpcframe.css" />
+    <link rel="stylesheet" type="text/css" href="../../_assets/css/defaultGpcframe.css" />
     <link rel="stylesheet" type="text/css" href="../../_assets/css/HighLightSelector.css" media="All" />
     <link rel="stylesheet" type="text/css" href="../../_assets/css/main.css" />
     <link rel="stylesheet" type="text/css" href="../../_assets/css/font-awesome.css" />
