0a779f81c56ef31caa4c61c0ca66e46fce0c1d57
Philippe Tourigny <philtourigny@gmail.com>
2018-06-17 23:57:50 -0400

    menu mobile (a finir)

--- _includes/footer/_footer.php
+++ _includes/footer/_footer.php
@@ -1,5 +1,5 @@
 <!--<link href="../../_assets/css/footer/_footer.css" rel="stylesheet" type="text/css"/>-->
-<div class="push">
+<div class="push"></div>
     <div class="pied">
         <div class="shrink clear">
             <div class="left">
@@ -17,4 +17,122 @@
             </div>
         </div>
     </div>
+<div id="ctl24_c78" class="N549e1a5b6f6f4a4d9bc7f62739bfc8ad_Ne394fb4275c44f1da5ddc6c65a3d1a3b menuMobile" data-staticClassNames="N549e1a5b6f6f4a4d9bc7f62739bfc8ad_Ne394fb4275c44f1da5ddc6c65a3d1a3b menuMobile">
+	<div id="ctl24_c78_c79" class="N549e1a5b6f6f4a4d9bc7f62739bfc8ad_N2d686881d1954ca1adbb2fa95aa02f31 bgblack" data-staticClassNames="N549e1a5b6f6f4a4d9bc7f62739bfc8ad_N2d686881d1954ca1adbb2fa95aa02f31 bgblack">
+		<div id="ctl24_c78_c79_ctl00_c17" class="N549e1a5b6f6f4a4d9bc7f62739bfc8ad_N384f9cf8833f44379cabc7ac83eba510_N776781fa500d4e4aa792bae6f1cb5678 recherche" data-staticClassNames="N549e1a5b6f6f4a4d9bc7f62739bfc8ad_N384f9cf8833f44379cabc7ac83eba510_N776781fa500d4e4aa792bae6f1cb5678 recherche">
+			<div id="ctl24_c78_c79_ctl00_c17_Na32e333ddb924e728779b52566acc13f" class="SearchMainDiv4fe851ec94e9431fb7ceba32177f8eba">
+				<input name="ctl00$ctl24$c78$c79$ctl00$c17$_txtSearchNa32e333ddb924e728779b52566acc13f" type="text" id="ctl24_c78_c79_ctl00_c17__txtSearchNa32e333ddb924e728779b52566acc13f" title="Rechercher" class="SearchTextBox4fe851ec94e9431fb7ceba32177f8eba" /><img id="ctl24_c78_c79_ctl00_c17_ctl00" class="SearchImage4fe851ec94e9431fb7ceba32177f8eba" src="../../_assets/images/menu_images/spacer.png" alt="Rechercher" style="cursor:pointer;" />
+			</div>
+		</div>
+	</div><div id="ctl24_c78_ctl00_c80" class="menuCss5">
+		<ul id="ctl24_c78_ctl00_c80Ul" class="MenuBar"><li class="item4 selected first rank0" id="c80_0" expansionmode="HoverExpansion"><a href="accueil.html" class="selected first rank0" target="_self">Accueil</a></li><li class="item13 rank1" id="c80_1" expansionmode="HoverExpansion"><a href="a-propos.html" class=" rank1" target="_self">&#192; propos</a></li><li class="item14 rank2 parent" id="c80_2" expansionmode="ClickExpansion"><a href="promotions-nouvelles/promotions.html" class=" rank2" target="_self">Promotions et nouvelles</a><div class="itemChildDiv14"><ul class="itemChild14"><li class="item46 first rank0" id="c80_2_0" expansionmode="HoverExpansion"><a href="promotions-nouvelles/promotions.html" class=" first rank0" target="_self">Promotions</a></li><li class="item47 rank1" id="c80_2_1" expansionmode="HoverExpansion"><a href="promotions-nouvelles/nouvelles.html" class=" rank1" target="_self">Nouvelles</a></li><li class="item79 rank2" id="c80_2_2" expansionmode="HoverExpansion"><a href="promotions-nouvelles/concours.html" class=" rank2" target="_self">Concours</a></li><li class="item93 last rank3" id="c80_2_3" expansionmode="HoverExpansion"><a href="promotions-nouvelles/promo-pieces.html" class=" last rank3" target="_self">Promo pi&#232;ces</a></li></ul></div></li><li class="item49 rank3" id="c80_3" expansionmode="HoverExpansion"><a href="urgence-routiere-24h.html" class=" rank3" target="_self">Urgence routi&#232;re 24h</a></li><li class="item5 rank4 parent" id="c80_4" expansionmode="ClickExpansion"><a href="camions-neufs/camions-lourds-neufs-international.html" class=" rank4" target="_self">Camions neufs</a><div class="itemChildDiv5"><ul class="itemChild5"><li class="item59 first rank0" id="c80_4_0" expansionmode="HoverExpansion"><a href="camions-neufs/inventaire-camion-neufs.html" class=" first rank0" target="_self">Inventaire complet</a></li><li class="item34 rank1" id="c80_4_1" expansionmode="HoverExpansion"><a href="camions-neufs/camions-lourds-neufs-international.html" class=" rank1" target="_self">Camions International</a></li><li class="item35 rank2" id="c80_4_2" expansionmode="HoverExpansion"><a href="camions-neufs/ottawa-kalmar.html" class=" rank2" target="_self">Camions Ottawa Kalmar</a></li><li class="item36 last rank3" id="c80_4_3" expansionmode="HoverExpansion"><a href="camions-neufs/isuzu.html" class=" last rank3" target="_self">Camions Isuzu</a></li></ul></div></li><li class="item62 rank5" id="c80_5" expansionmode="HoverExpansion"><a href="camions-occasion.html" class=" rank5" target="_self">Camions d&#39;occasion</a></li><li class="item63 rank6" id="c80_6" expansionmode="NoExpansion"><a href="remorques-neuves.html" class=" rank6" target="_self">Remorques neuves</a></li><li class="item9 rank7 parent" id="c80_7" expansionmode="ClickExpansion"><a href="vehicules-utilitaires/mini-excavatrices.html" class=" rank7" target="_self">V&#233;hicules utilitaires</a><div class="itemChildDiv9"><ul class="itemChild9"><li class="item37 first rank0" id="c80_7_0" expansionmode="HoverExpansion"><a href="vehicules-utilitaires/mini-excavatrices.html" class=" first rank0" target="_self">Mini-excavatrices</a></li><li class="item38 rank1" id="c80_7_1" expansionmode="HoverExpansion"><a href="vehicules-utilitaires/transporteurs-tout-terrain.html" class=" rank1" target="_self">Transporteurs tout-terrain</a></li><li class="item39 rank2" id="c80_7_2" expansionmode="HoverExpansion"><a href="vehicules-utilitaires/skid-steer-chargeur-chenilles.html" class=" rank2" target="_self">Skid Steer et chargeur &#224; chenilles</a></li><li class="item40 last rank3" id="c80_7_3" expansionmode="HoverExpansion"><a href="vehicules-utilitaires/chargeuses-yanmar-v3-v4.html" class=" last rank3" target="_self">Chargeuse V3 et V4</a></li></ul></div></li><li class="item10 rank8" id="c80_8" expansionmode="HoverExpansion"><a href="location-camions.html" class=" rank8" target="_self">Location de camions</a></li><li class="item11 rank9 parent" id="c80_9" expansionmode="ClickExpansion"><a href="pieces-services/financement.html" class=" rank9" target="_self">Pi&#232;ces et services</a><div class="itemChildDiv11"><ul class="itemChild11"><li class="item41 first rank0" id="c80_9_0" expansionmode="HoverExpansion"><a href="pieces-services/pieces-accessoires.html" class=" first rank0" target="_self">Pi&#232;ces et accessoires</a></li><li class="item42 rank1" id="c80_9_1" expansionmode="HoverExpansion"><a href="pieces-services/service-routier.html" class=" rank1" target="_self">Service routier</a></li><li class="item43 rank2" id="c80_9_2" expansionmode="HoverExpansion"><a href="pieces-services/apres-vente.html" class=" rank2" target="_self">Service apr&#232;s-vente</a></li><li class="item44 rank3" id="c80_9_3" expansionmode="HoverExpansion"><a href="pieces-services/financement.html" class=" rank3" target="_self">Financement</a></li><li class="item94 last rank4" id="c80_9_4" expansionmode="HoverExpansion"><a href="http://www.reseaudynamique.com/pieces-services/promo-pieces" class=" last rank4" target="_self">Promo pi&#232;ces</a></li></ul></div></li><li class="item12 last rank10" id="c80_10" expansionmode="NoExpansion"><a href="nous-joindre.html" class=" last rank10" target="_self">Nous joindre</a></li></ul>
+	</div>
 </div>
+
+<div id="ctl00_Nms_Designer_Web_UI_Widget_Zone_NmsLiteral61055d76e8bf4f1196d74c7b77f05a3b">	<script type="text/javascript">    //<![CDATA[
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+  $("body").attr("ontouchstart", "");
+  $("body").attr("onmouseover", "");
+
+  var $menuMobile = $(".menuMobile");
+  if ($(window).width() < 641) {
+    $menuMobile.css("left", -$menuMobile.outerWidth());
+  } else {
+    $menuMobile.hide();
+  }
+
+  $(".icoMenuMobile").click(function () {
+    $(this).toggleClass("open");
+
+    $menuMobile.animate({
+      left: parseInt($menuMobile.css('left'), 10) == 0 ?
+          -$menuMobile.outerWidth() :
+          0
+    });
+    $(".wrap").animate({
+      left: parseInt($menuMobile.css('left'), 10) == 0 ?
+        0 :
+        +$menuMobile.outerWidth()
+    });
+    $(".pied").animate({
+      left: parseInt($menuMobile.css('left'), 10) == 0 ?
+        0 :
+        +$menuMobile.outerWidth()
+    });
+  });
+
+  $(".menuCss5").find(".parent > a").click(function () {
+    var open = $(this).parent();
+    if (open.hasClass("expanded")) {
+      open.removeClass("expanded");
+      open.children("div").slideUp();
+    } else {
+      open.addClass("expanded");
+      open.children("div").slideDown("fast", function () {
+        $('html, body').animate({
+          scrollTop: open.offset().top
+        }, 1000);
+      });
+    }
+  });
+  //Faire afficher les filtres
+  $(".toggleFilters").bind("click", openFilters);
+  $(".viewResults a").bind("click", closeFilters);
+  function openFilters() {
+    $(".GpcMenuWrapper").slideDown();
+    $(".toggleFilters").addClass("selected").unbind("click").find(".openClose").bind("click", closeFilters);
+  }
+  function closeFilters() {
+    $(".GpcMenuWrapper").slideUp();
+    $(".toggleFilters").removeClass("selected");
+    setTimeout(function () {
+      $(".toggleFilters").bind("click", openFilters).find(".openClose").unbind("click")
+    }, 250)
+  }
+});
+
+var magicTimeout = "";
+
+$(window).resize(function () {
+  if (magicTimeout != null) {
+    clearTimeout(magicTimeout);
+    magicTimeout = null;
+  }
+  magicTimeout = setTimeout(function () {
+    var $menuMobile = $(".menuMobile");
+    if ($(window).width() < 641) {
+      $menuMobile.show();
+      if (parseInt($menuMobile.css("left")) < 0) {
+        $menuMobile.css("left", -$menuMobile.outerWidth())
+      } else {
+        if ($menuMobile.is(":visible")) {
+          $(".wrap").css("left", $menuMobile.outerWidth());
+          $(".pied").css("left", $menuMobile.outerWidth());
+        }
+      }
+    } else {
+      $menuMobile.hide();
+    }
+  }, 100);
+});
+    //]]>  </script></div><img alt="" src="#" style="position:absolute;left:-10px;top:-10px;width:1px;height:1px;" />
+
+	
+	
+<script type="text/javascript">
+//<![CDATA[
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','../www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-55593676-1', 'auto', {'name': 'nms'});
+ga('nms.send', 'pageview');$("img[src$='.png']").ready(function() {  if ($(this).height() > 0) { $(this).ifixpng(); }});$(document.body).bind('mouseover', function() { setTimeout(function() { $("img[src$='.png']").ifixpng(); }, 10); });$(document).HighlightSelector({QueryHashKeyName : 'qa',HighlightClass : 'qaHighlight'});$(document).ready(function(){});//]]>
+</script>
