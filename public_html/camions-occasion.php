49837dcb53c98b2c5850b609d4fcadb5f834f1c7
tartampion01 <philtourigny@gmail.com>
2018-06-19 13:53:16 -0400

    revert to repair used trucks

--- public_html/camions-occasion.php
+++ public_html/camions-occasion.php
@@ -1,5 +1,5 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/header/_header.php'); ?>
-<script type='text/javascript'>var _N = 0;</script>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/header/_headerUsed.php'); ?>
+<!--<script type='text/javascript'>var _N = 0;</script>-->
 <script src="_assets/js/camions-rest.js" type="text/javascript"></script>
 <body class="body"><?php RD_Utils::write_Gtag() ?>
     <form role="form" method="POST" action="/<?php echo $NOMPAGE; ?>">
@@ -17,9 +17,9 @@
                             <li class="GpcMenuCategory expanded">
                                 <a class="GpcMenuCategoryTitle" tabindex="">Marque</a>
                                 <ul class="marque" style="">
-                                    <?PHP $results = selectUsedTrucksDisctinctCriteria('marque', ' marque<>"capacity" and marque<>"asetrail" ', 'COUNT', 'DESC'); ?>
+                                    <?PHP $results = selectUsedTrucksDisctinctCriteria('marque', '' ); ?>
                                         <?PHP foreach($results as $key => $value){ ?>
-                                        <li class="GpcMenuItem filter-link" data-field='marque' data-value='<?PHP echo $key ?>' data-custom-criteria=' marque<>"capacity" and marque<>"asetrail" ' data-selected="false">
+                                        <li class="GpcMenuItem filter-link" data-field='marque' data-value='<?PHP echo $key ?>' data-custom-criteria='' data-selected="false">
                                             <?PHP /*echo "<a class='GpcItemTitle' href='?marque=$key'>$key</a><span class='GpcMenuItemCount'>($value)</span>";}*/ ?>
                                             <?PHP echo "<a class='GpcItemTitle' href='javascript:void(0);'>$key <span class='GpcMenuItemCount'>($value)</span></a>";} ?>
                                         </li>
@@ -29,9 +29,9 @@
                             <li class="GpcMenuCategory expanded">
                                 <a class="GpcMenuCategoryTitle" tabindex="">Modèle</a>
                                 <ul class="modele" style="">
-                                    <?PHP $results = selectUsedTrucksDisctinctCriteria('modele', ' marque<>"capacity" and marque<>"asetrail" ', 'modele', 'ASC'); ?>
+                                    <?PHP $results = selectUsedTrucksDisctinctCriteria('modele', ''); ?>
                                         <?PHP foreach($results as $key => $value){ ?>
-                                        <li class="GpcMenuItem filter-link" data-field='modele' data-value='<?PHP echo $key ?>' data-custom-criteria=' and marque<>"capacity" and marque<>"asetrail" ' data-selected="false">
+                                        <li class="GpcMenuItem filter-link" data-field='modele' data-value='<?PHP echo $key ?>' data-custom-criteria='' data-selected="false">
                                             <?PHP /*echo "<a class='GpcItemTitle' href='?modele=$key'>$key</a><span class='GpcMenuItemCount'>($value)</span>";}*/ ?>
                                             <?PHP echo "<a class='GpcItemTitle' href='javascript:void(0);'>$key <span class='GpcMenuItemCount'>($value)</span></a>";} ?>
                                         </li>
@@ -41,9 +41,9 @@
                             <li class="GpcMenuCategory expanded">
                                 <a class="GpcMenuCategoryTitle" tabindex="">Transmission</a>
                                 <ul class="transmission" style="">
-                                    <?PHP $results = selectUsedTrucksDisctinctCriteria('transmission', ' transmission<>"" AND marque<>"capacity" AND marque<>"asetrail" ', 'transmission', 'ASC'); ?>
+                                    <?PHP $results = selectUsedTrucksDisctinctCriteria('transmission', ''); ?>
                                         <?PHP foreach($results as $key => $value){ ?>
-                                        <li class="GpcMenuItem filter-link" data-field='transmission' data-value='<?PHP echo str_replace('+', '%2B', $key) ?>' data-custom-criteria=' transmission<>"" AND marque<>"capacity" AND marque<>"asetrail" ' data-selected="false">
+                                        <li class="GpcMenuItem filter-link" data-field='transmission' data-value='<?PHP echo str_replace('+', '%2B', $key) ?>' data-custom-criteria='' data-selected="false">
                                             <?PHP /*echo "<a class='GpcItemTitle' href='?transmission=$key'>$key</a><span class='GpcMenuItemCount'>($value)</span>";}*/ ?>
                                             <?PHP echo "<a class='GpcItemTitle' href='javascript:void(0);'>$key <span class='GpcMenuItemCount'>($value)</span></a>";} ?>
                                         </li>
@@ -53,9 +53,9 @@
                             <li class="GpcMenuCategory expanded">
                                 <a class="GpcMenuCategoryTitle" tabindex="">Moteur</a>
                                 <ul class="moteur" style="">
-                                    <?PHP $results = selectUsedTrucksDisctinctCriteria('moteur', ' moteur<>"" AND marque<>"capacity" AND marque<>"asetrail" ', 'moteur', 'ASC'); ?>
+                                    <?PHP $results = selectUsedTrucksDisctinctCriteria('moteur', ''); ?>
                                         <?PHP foreach($results as $key => $value){ ?>
-                                        <li class="GpcMenuItem filter-link" data-field='moteur' data-value='<?PHP echo $key ?>' data-custom-criteria=' moteur<>"" AND marque<>"capacity" AND marque<>"asetrail" ' data-selected="false">
+                                        <li class="GpcMenuItem filter-link" data-field='moteur' data-value='<?PHP echo $key ?>' data-custom-criteria='' data-selected="false">
                                             <?PHP /*echo "<a class='GpcItemTitle' href='?moteur=$key'>$key</a><span class='GpcMenuItemCount'>($value)</span>";}*/ ?>
                                             <?PHP echo "<a class='GpcItemTitle' href='javascript:void(0);'>$key <span class='GpcMenuItemCount'>($value)</span></a>";} ?>
                                         </li>
@@ -139,22 +139,23 @@
                             </div>
                         </div>
                     </div>
-                </div>                
+                </div>
+                
             </div>
             <div class="FacetedExplorerClear GpcClear"></div>
+            <div style="padding-left: 225px;padding:225px;">
+                <p>Réseau Dynamique est la référence dans le domaine des camions lourds et des tracteurs terminaux d'occasion.</p>
+                <h2>Pourquoi choisir un camion d'occasion chez Réseau Dynamique?</h2>
+                <p>Avant d'être mis en vente, tous les véhicules d'occasion de Réseau Dynamique ont été inspectés minutieusement et réparés. Revendeur de camions de toutes classes, vous êtes assuré de trouver un camion de grande qualité répondant en tous points à vos besoins d'affaires.</p>
+                <p>Détaillant de camions neufs de marque International, Réseau Dynamique offre des garanties prolongées du manufacturier sur les composants majeurs de ses véhicules. Les services d'experts des mécaniciens accrédités International offrent également la reconstruction complète des composants majeurs.</p>
+                <h2>Le service après-vente : la force d'un réseau</h2>
+                <p>Avec Réseau Dynamique, vous faites un choix sensé pour le présent et le futur. Les&nbsp;<a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::Root,page::NousJoindre); ?>" target="_self">19 centres de pièces et services du Réseau</a> dispersés sur le territoire québécois assurent aux clients de Réseau Dynamique un service après-vente inégalable. Remettez l'entretien de votre camion ou de votre flotte entre les mains de techniciens formés directement par les fabricants.</p>
+                <p>Disponibles en un seul endroit au Centre-du-Québec, venez faire l'essai de nos camions d'occasion.</p>
+            </div>
         </div>
-        <div style="padding-left: 225px;padding:225px;">
-            <p>Réseau Dynamique est la référence dans le domaine des camions lourds et des tracteurs terminaux d'occasion.</p>
-            <h2>Pourquoi choisir un camion d'occasion chez Réseau Dynamique?</h2>
-            <p>Avant d'être mis en vente, tous les véhicules d'occasion de Réseau Dynamique ont été inspectés minutieusement et réparés. Revendeur de camions de toutes classes, vous êtes assuré de trouver un camion de grande qualité répondant en tous points à vos besoins d'affaires.</p>
-            <p>Détaillant de camions neufs de marque International, Réseau Dynamique offre des garanties prolongées du manufacturier sur les composants majeurs de ses véhicules. Les services d'experts des mécaniciens accrédités International offrent également la reconstruction complète des composants majeurs.</p>
-            <h2>Le service après-vente : la force d'un réseau</h2>
-            <p>Avec Réseau Dynamique, vous faites un choix sensé pour le présent et le futur. Les&nbsp;<a name="hyperlien" href="<?php echo RD_PageLink::getHref(folder::Root,page::NousJoindre); ?>" target="_self">19 centres de pièces et services du Réseau</a> dispersés sur le territoire québécois assurent aux clients de Réseau Dynamique un service après-vente inégalable. Remettez l'entretien de votre camion ou de votre flotte entre les mains de techniciens formés directement par les fabricants.</p>
-            <p>Disponibles en un seul endroit au Centre-du-Québec, venez faire l'essai de nos camions d'occasion.</p>
-        </div>
-    </div>                
+    </div>
     </form>
-    <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/footer/_footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/footer/_footer.php'); ?>    
     
     <div class="loading-overlay">Loading&#8230;</div>
     
@@ -172,15 +173,17 @@
                         <h2>
                             <span class="ProductBrokerType_String">${marque}</span>
                             <span>&nbsp;</span>
-                            <span class="ProductBrokerType_String">${modele}</span>
+                            <span class="ProductBrokerType_String">${Model}</span>
                             <span>&nbsp;</span>
-                            <span class="ProductBrokerType_String">${config}</span>
+                            <span>
+                                <span class="ProductBrokerType_String">6 x 4</span>
+                            </span>
                         </h2>
                     </div>
                     <div class="customField">
                         <div class="forBroker label zoneForBroker">
                             <span class="forBroker label spanForBroker">No d'inventaire : </span>
-                            <span class="ProductBrokerType_String">${unite}</span>
+                            <span class="ProductBrokerType_String">${stock}</span>
                         </div>
                     </div>
                     <div class="customField">
@@ -192,13 +195,13 @@
                     <div class="customField">
                         <div class="forBroker">
                             <span class="forBroker">Modèle : </span>
-                            <span class="ProductBrokerType_String">${modele}</span>
+                            <span class="ProductBrokerType_String">${Model}</span>
                         </div>
                     </div>
                     <div class="customField">
                         <div class="forBroker">
                             <span class="forBroker">Moteur : </span>
-                            <span class="ProductBrokerType_String">${moteur}</span>
+                            <span class="ProductBrokerType_String">${engine}</span>
                         </div>
                     </div>
                     <div class="inPromo">
@@ -215,7 +218,7 @@
     $( document ).ready(function() {
         
         // On page load, fetch all records
-        fetchRecords('', '', '  marque<>"capacity" and marque<>"asetrail" ', true, 0);
+        fetchRecords('', '', '', true, 0);        
     });
     </script>
 </body>
