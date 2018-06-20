40b1fbb7ee9cab60baacebc48144bb708c910d90
jpbernard <jpbernard@Tech-PC-10.sinfo.local>
2018-06-19 21:43:15 -0400

    Fix home footer

--- public_html/accueil.php
+++ public_html/accueil.php
@@ -1,6 +1,6 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/header/_header.php'); ?>
 <script type='text/javascript' src='../../_assets/js/jquery.jshowoff.js'></script>
-<body class="body"><?php RD_Utils::write_Gtag() ?>
+<body class="body home-page"><?php RD_Utils::write_Gtag() ?>
     <form role="form" method="POST" action="/<?php echo $NOMPAGE; ?>">
     <div class="wrap" data-staticClassNames="wrap">
         <div class="banner" data-staticClassNames="banner">
