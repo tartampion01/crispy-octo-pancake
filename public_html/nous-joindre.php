318a4d4940bcf1dc80f42ead5ac065f6186b8319
jpbernard <jpbernard@Tech-PC-10.sinfo.local>
2018-06-22 19:06:47 -0400

    Remove duplicated Google Analytic script

--- public_html/nous-joindre.php
+++ public_html/nous-joindre.php
@@ -935,14 +935,6 @@ function urldecode(str) {
 </div>
 </div>
 </div>
-        
-<script type="text/javascript">
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-120787761-1', 'auto');
-</script>
 
     </form>
     <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/footer/_footer.php'); ?>
--- public_html/promotions-nouvelles/promo-pieces.php
+++ public_html/promotions-nouvelles/promo-pieces.php
@@ -89,14 +89,6 @@
 </div>
 <img alt="" src="#" style="position:absolute;left:-10px;top:-10px;width:1px;height:1px;" />
 
-<script type="text/javascript">
-//<![CDATA[
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-120787761-1', 'auto');
-</script>
 </form>
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/footer/_footer.php'); ?>
 </body>
