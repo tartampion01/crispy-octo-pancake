a7cf86dcdf4e299db35281b83861cc339065d79e
tartampion01 <philtourigny@gmail.com>
2019-01-01 10:11:11 -0500

    update circulaire jan-fev 2019

---
+++ public_html/promotions-nouvelles/promo-pieces-pdf_1.php
@@ -0,0 +1,5 @@
+<head></head>
+<body class="body">
+    <object data="../_assets/Promo Janvier-Février 2019 - Réseau Dynamique.pdf" type="application/pdf" width="100%" height="100%"></object>
+</body>
+</html>
