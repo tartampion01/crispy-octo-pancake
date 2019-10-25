9402e9dbfb5e5c6e82b06667cbc42b7a68627209
WX3dev <dev@wx3data.com>
2019-10-25 14:23:19 -0400

    menu wx3

--- _includes/header/_header_main_menu.php
+++ _includes/header/_header_main_menu.php
@@ -1,5 +1,5 @@
 <?php
-$pageName = basename($_SERVER['SCRIPT_NAME']);
+/*$pageName = basename($_SERVER['SCRIPT_NAME']);
 $section = '';
 
 if(strpos($_SERVER['REQUEST_URI'], 'camions-neufs/') !== false) {
@@ -22,9 +22,10 @@ elseif(strpos($_SERVER['REQUEST_URI'], 'pieces-services/') !== false || strpos($
 }
 elseif(strpos($_SERVER['REQUEST_URI'], 'carrieres.php') !== false) {
     $section = 'carrieres';
-}
+}*/
 ?>
 <!-- MAIN MENU -->
+<!--
 <div id="menuPrincipal" class="menuprincipal">
     <div id="menuShrinkClear" class="shrink clear">
         <div id="menuCss1" class="menuCss1">
@@ -120,18 +121,17 @@ elseif(strpos($_SERVER['REQUEST_URI'], 'carrieres.php') !== false) {
             </ul>
         </div>
     </div>
-</div>
-
+</div>-->
 <?php
-if($section == 'camion-neufs' || $section == 'camions-occasion' || $section == 'remorques-neuves') {
+//if($section == 'camion-neufs' || $section == 'camions-occasion' || $section == 'remorques-neuves') {
 ?>
+<!--Mobile menu-->
+<!--
 <div class="menu-mobile-filters">
     <a class="m toggleFilters" target="_self">
         <span class="txt">Filtrer les résultats</span>
         <span class="search">
             <div data-search="true">
-                <!--<input id="" name="" type="text" class="GpcSearchInput" placeholder="Filtrer les résultats">
-                <img class="GpcSearchButton" name="libraryimage" title="" src="../../_assets/images/loupe-mobile.jpg" style="cursor: pointer;">-->
                 <form role="form" method="POST" action="../search.php">
                     <input id="tbSearch" name="tbSearch" title="Rechercher" class="GpcSearchInput" placeholder="Filtrer les résultats" type="text">
                     <img class="GpcSearchButton" name="libraryimage" title="" src="../../_assets/images/loupe-mobile.jpg" style="cursor: pointer;">
@@ -140,7 +140,182 @@ if($section == 'camion-neufs' || $section == 'camions-occasion' || $section == '
         </span>
         <span class="openClose"></span>
     </a>
-</div>
+</div>-->
 <?php
 }
 ?>
+<!--Main Menu WX3-->
+<nav class="bgnav hide-on-mobile">
+                <div class="menudestop grid grid-pad">
+                    <ul>
+                        <li>
+                            <a href="filter.html">Camions</a>
+                            <ul class="hidden">
+                                <li>
+                                    <a href="filter.html">International</a>
+                                    <ul class="hidden">
+                                        <li>
+                                            <a href="#">série CV</a>
+                                            <ul class="hidden">
+                                                <li>
+                                                    <img src="Resources/images/CV-navigation.jpg" alt="" />
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a href="https://www.internationaltrucks.com/configurator/cv" target="_blank">Configurer<br />votre véhicule</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3" style="border-left:solid 2px black;border-right:solid 2px black;">
+                                                        <a style="margin-top:9px;" href="https://www.internationaltrucks.com/trucks/cv-series" target="_blank">Explorer</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a style="margin-top:9px;" href="filter.html">Inventaire</a>
+                                                    </h4>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="#">série HV</a>
+                                            <ul class="hidden">
+                                                <li>
+                                                    <img src="Resources/images/HV-navigation.jpg" alt="" />
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a href="https://www.internationaltrucks.com/configurator/hv" target="_blank">Configurer<br />votre véhicule</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3" style="border-left:solid 2px black;border-right:solid 2px black;">
+                                                        <a style="margin-top:9px;" href="https://www.internationaltrucks.com/trucks/hv-series" target="_blank">Explorer</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a style="margin-top:9px;" href="filter.html">Inventaire</a>
+                                                    </h4>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="#">série HX</a>
+                                            <ul class="hidden">
+                                                <li>
+                                                    <img src="Resources/images/hx-navigation.jpg" alt="" />
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a href="https://www.internationaltrucks.com/configurator/hx" target="_blank">Configurer<br />votre véhicule</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3" style="border-left:solid 2px black;border-right:solid 2px black;">
+                                                        <a style="margin-top:9px;" href="https://www.internationaltrucks.com/trucks/hx-series" target="_blank">Explorer</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a style="margin-top:9px;" href="filter.html">Inventaire</a>
+                                                    </h4>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="#">série LT</a>
+                                            <ul class="hidden">
+                                                <li>
+                                                    <img src="Resources/images/lt-navigation.png" alt="" />
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a href="https://www.internationaltrucks.com/configurator/lt" target="_blank">Configurer<br />votre véhicule</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3" style="border-left:solid 2px black;border-right:solid 2px black;">
+                                                        <a style="margin-top:9px;" href="https://www.internationaltrucks.com/trucks/lt-series" target="_blank">Explorer</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a style="margin-top:9px;" href="filter.html">Inventaire</a>
+                                                    </h4>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="#">série MV</a>
+                                            <ul class="hidden">
+                                                <li>
+                                                    <img src="Resources/images/MV-navigation.jpg" alt="" />
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a href="https://www.internationaltrucks.com/configurator/mv" target="_blank">Configurer<br />votre véhicule</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3" style="border-left:solid 2px black;border-right:solid 2px black;">
+                                                        <a style="margin-top:9px;" href="https://www.internationaltrucks.com/trucks/mv-series" target="_blank">Explorer</a>
+                                                    </h4>
+                                                    <h4 class="col-1-3 mobile-col-1-3">
+                                                        <a style="margin-top:9px;" href="filter.html">Inventaire</a>
+                                                    </h4>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="filter.html">Isuzu</a>
+                                </li>
+                                <li>
+                                    <a href="filter.html">Occasion</a>
+                                </li>
+                                <li>
+                                    <a href="filter.html">Inventaire</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">pièces</a>
+                            <ul class="hidden">
+                                <li><a href="filter.html">Nouveauté</a></li>
+                                <li><a href="filter.html">Occasion</a></li>
+                                <li><a href="filter.html">Boutique en ligne</a></li>
+                                <li><a href="filter.html">Inventaire</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Service</a>
+                            <ul class="hidden">
+                                <li><a href="#">Routier</a></li>
+                                <li><a href="#">Après-Vente</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Financement</a>
+                            <ul class="hidden">
+                                <li><a href="#">Long terme</a></li>
+                                <li><a href="#">Court terme</a></li>
+                                <li><a href="#">Calculateur</a></li>
+                                <li><a href="#">Commande en ligne</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Location</a>
+                            <ul class="hidden">
+                                <li><a href="#">Court terme</a></li>
+                                <li><a href="#">Long terme</a></li>
+                                <li><a href="#">Avec entretien</a></li>
+                                <li><a href="#">Sans entretien</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Carrière</a>
+                            <ul class="hidden">
+                                <li><a href="#">Emploi disponible</a></li>
+                                <li><a href="#">Canditature spontanée</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Circulaire</a>
+                            <ul class="hidden">
+                                <li><a href="#">Promotion</a></li>
+                                <li style="min-width:230px;"><a href="#">Circulaire complet</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">À Propos</a>
+                            <ul class="hidden" style="min-width:210px;">
+                                <li><a href="#">Notre entreprise</a></li>
+                                <li><a href="#">Notre réseaux</a></li>
+                                <li><a href="filter.html">Occasion</a></li>
+                                <li><a href="filter.html">Inventaire</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Nous Joindre</a>
+                            <ul class="hidden">
+                                <li><a href="#map">Concessionaire</a></li>
+                                <li style="min-width:230px;"><a href="#map">Point de service</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
