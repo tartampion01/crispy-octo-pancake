fcace8e89fb0c011a616ecb9de0c24a66c5db8e7
tartampion01 <philtourigny@gmail.com>
2019-11-08 20:47:09 -0500

    pages pour manipuler JSON pour refonte page détail des camions

---
+++ public_html/api/singleNewTruck_.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-Type: application/json');
+
+    if( isset($_GET["id"])){
+        require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/commonIncludes.php');
+    }
+    else
+        header("Location: accueil.php");
+
+$camion = new RD_Camion(null);
+$camion->load_new(base64_decode(urldecode($_GET["id"])));
+
+echo json_encode($camion);
+?>
