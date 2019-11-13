cf3fd0b0b7adc4803e5fb005c428b5fd059b7ad9
dev <dev@wx3data.com>
2019-11-13 11:10:49 -0500

    add json test page

--- public_html/Test.php
+++ public_html/Test.php
@@ -8,68 +8,66 @@
 <html>
 
 <head>
-<title>Test Json</title>
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-<!-- <script src="https://cdn.jsdelivr.net/npm/vue"></script> -->
+    <title>Test Json</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/vue"></script> -->
 
-<script type="text/javascript">
+    <script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function(event) {
+        init();
+    });
 
-document.addEventListener("DOMContentLoaded", function(event) { init(); });
+    function init() {
 
-function init(){
+        var vm = new Vue({
+            el: '#detail',
+            data: {
+                message: 'Hello Vue!',
+                item: {}
+            },
+            mounted: function() {
+                this.message = "Ho !";
+                try {
 
-  var vm = new Vue({
-  el: '#detail',
-  data: {
-    message: 'Hello Vue!',
-    item: {}
-  },
-  mounted : function() {
-    this.message = "Ho !";
-    try {
+                    this.readData();
 
-        this.readData();
+                } catch (error) {
+                    console.error('Error:', error);
+                }
+            },
+            methods: {
+                async readData() {
 
-        } catch (error) {
-            console.error('Error:', error);
-        }
-  },
-  methods:{
-      async readData(){
+                    let id = (window.location.search.match(new RegExp('[?&]' + 'id' + '=([^&]+)')) || [,null])[1];
+                    if (id) {
 
-        let id = (window.location.search.match(new RegExp('[?&]' + 'id' + '=([^&]+)')) || [, null])[1] ;
-        if(id) {
+                        // fetch('http://reseaudynamique.com/api/singleNewTruck_.php?id=NjIyNQ==')
+                        // .then(response => response.json())
+                        // .then(data => { this.item = data; });
 
-        // fetch('http://reseaudynamique.com/api/singleNewTruck_.php?id=NjIyNQ==')
-        // .then(response => response.json())
-        // .then(data => { this.item = data; });
-
-        const response = await fetch('http://reseaudynamique.com/api/singleNewTruck_.php?id=' + id)
-        const data = await response.json()
-        this.item = data;
-        console.log(data);
-
-        }
-      }
-  }
-})
-}
-
-</script>
+                        const response = await fetch(
+                            'http://reseaudynamique.com/api/singleNewTruck_.php?id=' + id)
+                        const data = await response.json()
+                        this.item = data;
+                        console.log(data);
+                    }
+                }
+            }
+        })
+    }
+    </script>
 
 </head>
 
 <body>
-<!-- <?php echo "allo"; /*phpinfo();*/ ?> -->
+    <!-- <?php echo "allo"; /*phpinfo();*/ ?> -->
 
-<div id="detail">
-{{ message }}<br /><br />
+    <div id="detail">
+        {{ message }}<br /><br />
 
-<span v-html="item.beauTitre" ></span>
-
-</div>
+        <span v-html="item.beauTitre"></span>
+    </div>
 
 </body>
 
 </html>
-
