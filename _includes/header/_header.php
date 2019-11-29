66d9785a8c1835a9fd82a6ce31b4b0fa4c701ceb
WX3dev <dev@wx3data.com>
2019-11-29 09:01:32 -0500

    changement header

--- _includes/header/_header.php
+++ _includes/header/_header.php
@@ -89,7 +89,7 @@
                 infinite: true,
                 responsive: [
                     {
-                        breakpoint: 1024,
+                        breakpoint: 1200,
                         settings: {
                             slidesToShow: 3,
                             slidesToScroll: 3,
@@ -98,14 +98,14 @@
                         }
                     },
                     {
-                        breakpoint: 600,
+                        breakpoint: 1024,
                         settings: {
                             slidesToShow: 2,
                             slidesToScroll: 2
                         }
                     },
                     {
-                        breakpoint: 480,
+                        breakpoint: 600,
                         settings: {
                             slidesToShow: 1,
                             slidesToScroll: 1
