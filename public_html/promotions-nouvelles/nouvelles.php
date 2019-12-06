5637868f5f50f083fb264974b751199299ea2ca9
Dev <dev@wx3data.com>
2019-12-06 12:45:42 -0500

    arranger pages promo +

--- public_html/promotions-nouvelles/nouvelles.php
+++ public_html/promotions-nouvelles/nouvelles.php
@@ -1,9 +1,9 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/header/_header.php'); ?>
 <body class="body"><?php RD_Utils::write_Gtag() ?>
     <form role="form" method="POST" action="/promotions-nouvelles/<?php echo $NOMPAGE; ?>">
-    <div class="">
+    <div class="wrap">
         <div class="content">
-            <div class="shrink">
+            <div class="grid grid-pad">
                 <div class="titrepage">
                     <h1>Nouvelles</h1>
                 </div>
