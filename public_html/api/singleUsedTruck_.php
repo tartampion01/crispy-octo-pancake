371122fb638b92797d4303cd7c1d15f9bdf0837b
dev <dev@wx3data.com>
2020-01-07 13:20:07 -0500

    add single truck api

---
+++ public_html/api/singleUsedTruck_.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+    if( isset($_GET["id"])){
+        require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/commonIncludes.php');
+    }
+    else
+        header("Location: accueil.php");
+
+$camion = new RD_Camion(null);
+$camion->load_used(base64_decode(urldecode($_GET["id"])));
+
+echo json_encode($camion);
+?>
