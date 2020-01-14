05994a5831fa820ea6649d4e7a07aa0846aab398
Dev <dev@wx3data.com>
2020-01-14 08:56:29 -0500

    mardi matin pull

--- _includes/slider/multi_item_product.php
+++ _includes/slider/multi_item_product.php
@@ -1,18 +1,33 @@
-<section class="grid ">
+<section class="grid">
         <div class="grid-pad">
             <h2 class="titlesection">Nos Spéciaux</h2>
         </div>
         <section class="realization">
+        <div class="image-box rel">
+                <a href="./Fleuristebergeron.html" title="">
+                    <div class="rectangle"></div>
+                    <div class="priceColor"> 
+
+                    <img src="../../_assets/images/wx3/ltserie.png" alt="">
+                    </div>
+                    <h2 class="uppercases">Le Gros Truck Rouge</h2>
+                    <hr />
+                    <p style="text-align:justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    <div class="price">80 000$</div>
+                </a>
+            </div>
             <div class="image-box">
                 <a href="./Fleuristebergeron.html" title="">
                     <div class="rectangle"></div>
+                    <div class="priceColor">
                     <img src="../../_assets/images/wx3/hxserie.png" alt="">
+                    </div>
                     <h2 class="uppercases">Titre de camion</h2>
                     <hr />
                     <p style="text-align:justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
                 </a>
             </div>
-            <div class="">
+            <div class=" ">
                 <a href="./Fleuristebergeron.html" title="">
                     <div class="rectangle"></div>
                     <img src="../../_assets/images/wx3/promo.jpg" alt="">
@@ -20,14 +35,52 @@
                     <h2 class="uppercases">circulaire</h2>
                 </a>
             </div>
-            <div class="image-box">
-                <a href="./Fleuristebergeron.html" title="">
+            <div class="image-box rel">
+            <a href="./Fleuristebergeron.html" title="">
+                <div class="rectangle"></div>
+                <img src="../../_assets/images/wx3/ltserie.png" alt="">
+                <div class="text-block">
+                <h4>Nature</h4>
+                <p>What a beautiful sunrise</p>
+                </div>
+                <div>
+                <h2 class="uppercases">Titre de camion</h2>
+                    <hr />
+                    <p style="text-align:justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+</div>
+</a>
+            </div>
+            <div class="">
+             
                     <div class="rectangle"></div>
+                    <div class="priceColor">
                     <img src="../../_assets/images/wx3/ltserie.png" alt="">
+                    </div>
                     <h2 class="uppercases">Titre de camion</h2>
                     <hr />
                     <p style="text-align:justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
                 </a>
+            </div>
+
+            <div class="promo_box">
+            <a href="./Fleuristebergeron.html" title="">
+            <div class="promo_top_orange"></div>
+            <div class="promo_img">
+                <div class="promo_box_triangle">
+            <img src="../../_assets/images/wx3/ltserie.png" alt="">
+            </div>
+                <h4>Nature</h4>
+                <p>What a beautiful sunrise</p>
+                
+            </div>
+        allo
+            </a>
+            </div>
+            <div class="bgtrucks">
+            <div class="rectangle"></div>
+                          <div class="promo_ ">
+                        <img src="../../_assets/images/wx3/ltserie.png" alt=""  >
+             </div>
             </div>
         </section>
     </section>
