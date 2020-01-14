a78f6c5bee948672088380f87148d716d4fb5dca
Dev <dev@wx3data.com>
2020-01-14 11:59:13 -0500

    section promo

--- _includes/slider/multi_item_product.php
+++ _includes/slider/multi_item_product.php
@@ -1,86 +1,47 @@
-<section class="grid">
-        <div class="grid-pad">
-            <h2 class="titlesection">Nos Spéciaux</h2>
-        </div>
-        <section class="realization">
-        <div class="image-box rel">
+<section class="realization">
+        <div class="promo_box">
                 <a href="./Fleuristebergeron.html" title="">
-                    <div class="rectangle"></div>
-                    <div class="priceColor"> 
-
-                    <img src="../../_assets/images/wx3/ltserie.png" alt="">
-                    </div>
-                    <h2 class="uppercases">Le Gros Truck Rouge</h2>
-                    <hr />
-                    <p style="text-align:justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                    <div class="price">80 000$</div>
-                </a>
-            </div>
-            <div class="image-box">
-                <a href="./Fleuristebergeron.html" title="">
-                    <div class="rectangle"></div>
-                    <div class="priceColor">
-                    <img src="../../_assets/images/wx3/hxserie.png" alt="">
-                    </div>
-                    <h2 class="uppercases">Titre de camion</h2>
-                    <hr />
-                    <p style="text-align:justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-                </a>
-            </div>
-            <div class=" ">
-                <a href="./Fleuristebergeron.html" title="">
-                    <div class="rectangle"></div>
-                    <img src="../../_assets/images/wx3/promo.jpg" alt="">
-                    <hr style="margin-top:5px;" />
-                    <h2 class="uppercases">circulaire</h2>
-                </a>
-            </div>
-            <div class="image-box rel">
-            <a href="./Fleuristebergeron.html" title="">
-                <div class="rectangle"></div>
-                <img src="../../_assets/images/wx3/ltserie.png" alt="">
-                <div class="text-block">
-                <h4>Nature</h4>
-                <p>What a beautiful sunrise</p>
+                    <div class="promo_top_orange"></div>
+                    <div class="promo_img promo_cover">
+                    <img class="promo_fit" src="http://raisindynamique.reseaudynamique.com/_assets/images/wx3/promo.jpg" alt="">   
+                   
                 </div>
-                <div>
-                <h2 class="uppercases">Titre de camion</h2>
-                    <hr />
-                    <p style="text-align:justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-</div>
-</a>
+                    <h2 class="uppercases">Titre de camion</h2>     
+                 </a>
             </div>
-            <div class="">
-             
-                    <div class="rectangle"></div>
-                    <div class="priceColor">
-                    <img src="../../_assets/images/wx3/ltserie.png" alt="">
-                    </div>
-                    <h2 class="uppercases">Titre de camion</h2>
-                    <hr />
-                    <p style="text-align:justify;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                </a>
-            </div>
-
             <div class="promo_box">
-            <a href="./Fleuristebergeron.html" title="">
-            <div class="promo_top_orange"></div>
-            <div class="promo_img">
-                <div class="promo_box_triangle">
-            <img src="../../_assets/images/wx3/ltserie.png" alt="">
+                <a href="./Fleuristebergeron.html" title="">
+                    <div class="promo_top_orange"></div>
+                    <div class="promo_img promo_cover">
+                    <img class="promo_fit" src="http://raisindynamique.reseaudynamique.com/api/pictures.php?n=1&id=8243" alt="">   
+                    <div class="promo_box_triangle"></div>
+                    <div class="promo_price">180 000$</div>
+                </div>
+                    <h2 class="uppercases">Titre de camion</h2>     
+                 </a>
             </div>
-                <h4>Nature</h4>
-                <p>What a beautiful sunrise</p>
-                
+            <div class="promo_box">
+                <a href="./Fleuristebergeron.html" title="">
+                    <div class="promo_top_orange"></div>
+                    <div class="promo_img promo_cover">
+                    <img class="promo_fit" src="http://raisindynamique.reseaudynamique.com/_assets/images/wx3/hxserie.png" alt="">   
+                    <div class="promo_box_triangle"></div>
+                    <div class="promo_price">80 000$</div>
+                </div>
+                    <h2 class="uppercases">Titre de camion</h2>     
+                 </a>
             </div>
-        allo
-            </a>
+            <div class="promo_box">
+                <a href="./Fleuristebergeron.html" title="">
+                    <div class="promo_top_orange"></div>
+                    <div class="promo_img promo_cover">
+                    <img class="promo_fit" src="http://raisindynamique.reseaudynamique.com/api/pictures.php?n=1&id=8243" alt="">   
+                    <div class="promo_box_triangle"></div>
+                    <div class="promo_price">80 000$</div>
+                </div>
+                    <h2 class="uppercases">Titre de camion</h2>     
+                 </a>
             </div>
-            <div class="bgtrucks">
-            <div class="rectangle"></div>
-                          <div class="promo_ ">
-                        <img src="../../_assets/images/wx3/ltserie.png" alt=""  >
-             </div>
-            </div>
+           
         </section>
-    </section>
+ 
