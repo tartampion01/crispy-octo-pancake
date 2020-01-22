52b9124bce68217cc9f6c17417dea64488140200
WX3dev <dev@wx3data.com>
2020-01-22 15:33:08 -0500

    changement banner

--- _includes/slider/home_banner.php
+++ _includes/slider/home_banner.php
@@ -13,13 +13,13 @@ $(document).ready(function () {
 <section id="diapo">
     <div class="diapo fixedheader">
         <!-- Fixing diapo slick  *class*** diapo fixedheader col-1-1 mobile-col-1-1  *** -->
-        <div class="slides_container"><img src="../../_assets/images/wx3/diapo/diapo1.jpg" alt="" /></div>
-        <div class="slides_container"><img src="../../_assets/images/wx3/diapo/diapo2.jpg" alt="" /></div>
-        <div class="slides_container"><img src="../../_assets/images/wx3/diapo/diapo3.jpg" alt="" /></div>
-        <div class="slides_container"><img src="../../_assets/images/wx3/diapo/diapo4.jpg" alt="" /></div>
-        <div class="slides_container"><img src="../../_assets/images/wx3/diapo/diapo5.png" alt="" /></div>
-        <div class="slides_container"><img src="../../_assets/images/wx3/diapo/diapo6.png" alt="" /></div>
-        <div class="slides_container"><img src="../../_assets/images/wx3/diapo/diapo7.jpg" alt="" /></div>
-        <div class="slides_container"><img src="../../_assets/images/wx3/diapo/diapo8.png" alt="" /></div>
+        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo1.png" alt="" /></a></div>
+        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo2.jpg" alt="" /></a></div>
+        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo3.jpg" alt="" /></a></div>
+        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo4.jpg" alt="" /></a></div>
+        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo5.png" alt="" /></a></div>
+        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo6.png" alt="" /></a></div>
+        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo7.jpg" alt="" /></a></div>
+        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo8.jpg" alt="" /></a></div>
     </div>
 </section>
