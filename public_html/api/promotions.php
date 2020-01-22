4e4033a2e8fa54cbd5e9c924ae4035d4e3937c4b
dev <dev@wx3data.com>
2020-01-22 16:33:05 -0500

    correction fr

--- public_html/api/promotions.php
+++ public_html/api/promotions.php
@@ -3,7 +3,7 @@
     header("Content-type: application/json");
 ?>
 [
-    { "Brochure" : {"title":"le circulaire", "imgSource": "../../_assets/images/promo/reseau-dynamique-promo.jpg", "url":"/promotions-nouvelles/promo-pieces.php"} },
+    { "Brochure" : {"title":"la circulaire", "imgSource": "../../_assets/images/promo/reseau-dynamique-promo.jpg", "url":"/promotions-nouvelles/promo-pieces.php"} },
     { "Truck": { "id" : "6391", "title":"International - LT", "picture_id":"8243", "Price" : "55 000$" } },
     { "Truck": { "id" : "6392", "title":"International - MV607 SBA","picture_id":"8244", "Price" : "45 000$" } },
     { "Truck": { "id" : "6392", "title":"International - MV607 SBA","picture_id":"8244", "Price" : "35 000$" } }
