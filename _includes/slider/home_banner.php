ad5bdbf7e54c9beb1d4406dc890326678da08bd5
WX3dev <dev@wx3data.com>
2020-01-22 15:43:04 -0500

    changement diapo et icon

--- _includes/slider/home_banner.php
+++ _includes/slider/home_banner.php
@@ -13,13 +13,13 @@ $(document).ready(function () {
 <section id="diapo">
     <div class="diapo fixedheader">
         <!-- Fixing diapo slick  *class*** diapo fixedheader col-1-1 mobile-col-1-1  *** -->
-        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo1.png" alt="" /></a></div>
-        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo2.jpg" alt="" /></a></div>
-        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo3.jpg" alt="" /></a></div>
-        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo4.jpg" alt="" /></a></div>
-        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo5.png" alt="" /></a></div>
-        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo6.png" alt="" /></a></div>
-        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo7.jpg" alt="" /></a></div>
-        <div class="slides_container"><a><img src="../../_assets/images/wx3/diapo/diapo8.jpg" alt="" /></a></div>
+        <div class="slides_container"><a href="https://www.internationaltrucks.com/engines/international-a26" target="_blank"><img src="../../_assets/images/diapo/diapo1.png" alt="" /></a></div>
+        <div class="slides_container"><a href="/trucks.php?n=1&params=B_Inte"><img src="../../_assets/images/diapo/diapo2.jpg" alt="" /></a></div>
+        <div class="slides_container"><a href="/trucks.php?n=1&params=B_Inte"><img src="../../_assets/images/diapo/diapo3.jpg" alt="" /></a></div>
+        <div class="slides_container"><a href="/trucks.php?n=1&params=B_Inte"><img src="../../_assets/images/diapo/diapo4.jpg" alt="" /></a></div>
+        <div class="slides_container"><a href="/trucks.php?n=1&params=B_Inte"><img src="../../_assets/images/diapo/diapo5.png" alt="" /></a></div>
+        <div class="slides_container"><a href="/promotions-nouvelles/promo-pieces.php"><img src="../../_assets/images/diapo/diapo6.png" alt="" /></a></div>
+        <div class="slides_container"><a href="/trucks.php?n=1&params=B_Isuz"><img src="../../_assets/images/diapo/diapo7.jpg" alt="" /></a></div>
+        <div class="slides_container"><a href="/trucks.php?n=1&params=B_Inte"><img src="../../_assets/images/diapo/diapo8.jpg" alt="" /></a></div>
     </div>
 </section>
