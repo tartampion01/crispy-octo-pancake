71c4060a1698544b1ef839544601268be9e5ac94
WX3dev <dev@wx3data.com>
2020-04-28 09:54:06 -0400

    video

--- public_html/accueil.php
+++ public_html/accueil.php
@@ -65,6 +65,52 @@
     <!--multi-item build your own truck-->
     <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/../_includes/slider/multi_item_build_truck.php'); ?>  
 
+    <!--youtube video-->
+    <section>
+        <div class="grid grid-pad">
+            <h1 class="">Le nouveau International CV515 2020 Classe 5</h1>
+         </div>
+         <style>
+            .hytPlayerWrap{
+                position: relative;
+            }
+            .hytPlayerWrap.ended::after{
+                content:""; 
+                position: absolute; 
+                top: 0; 
+                left: 0; 
+                bottom: 0; 
+                right: 0; 
+                cursor: pointer;
+                background-color: black; 
+                background-repeat: no-repeat; 
+                background-position: center; 
+                background-size: 64px 64px; 
+                background-image: url(data:image/svg+xml;utf8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMjgiIGhlaWdodD0iMTI4IiB2aWV3Qm94PSIwIDAgNTEwIDUxMCI+PHBhdGggZD0iTTI1NSAxMDJWMEwxMjcuNSAxMjcuNSAyNTUgMjU1VjE1M2M4NC4xNSAwIDE1MyA2OC44NSAxNTMgMTUzcy02OC44NSAxNTMtMTUzIDE1My0xNTMtNjguODUtMTUzLTE1M0g1MWMwIDExMi4yIDkxLjggMjA0IDIwNCAyMDRzMjA0LTkxLjggMjA0LTIwNC05MS44LTIwNC0yMDQtMjA0eiIgZmlsbD0iI0ZGRiIvPjwvc3ZnPg==);
+            }
+            .hytPlayerWrap.paused::after{
+                    content:""; 
+                    position: absolute; 
+                    top: 70px; 
+                    left: 0; 
+                    bottom: 50px; 
+                    right: 0; 
+                    cursor: pointer; 
+                    background-color: black; 
+                    background-repeat: no-repeat;
+                    background-position: center; 
+                    background-size: 40px 40px; 
+                    background-image: url(data:image/svg+xml;utf8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEiIHdpZHRoPSIxNzA2LjY2NyIgaGVpZ2h0PSIxNzA2LjY2NyIgdmlld0JveD0iMCAwIDEyODAgMTI4MCI+PHBhdGggZD0iTTE1Ny42MzUgMi45ODRMMTI2MC45NzkgNjQwIDE1Ny42MzUgMTI3Ny4wMTZ6IiBmaWxsPSIjZmZmIi8+PC9zdmc+);
+            }
+        </style>
+        <div class="hytPlayerWrapOuter"> 
+            <div class="hytPlayerWrap"> 
+            <iframe width="100%" height="600" id="widget2" src="https://www.youtube.com/embed/bxod0lSB-kg?rel=0&amp;enablejsapi=1&amp;mute=1" frameborder="0"></iframe>
+            </div>
+        </div>
+        <script>"use strict"; document.addEventListener('DOMContentLoaded', function(){if (window.hideYTActivated) return; let onYouTubeIframeAPIReadyCallbacks=[]; for (let playerWrap of document.querySelectorAll(".hytPlayerWrap")){let playerFrame=playerWrap.querySelector("iframe"); let tag=document.createElement('script'); tag.src="https://www.youtube.com/iframe_api"; let firstScriptTag=document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); let onPlayerStateChange=function(event){if (event.data==YT.PlayerState.ENDED){playerWrap.classList.add("ended");}else if (event.data==YT.PlayerState.PAUSED){playerWrap.classList.add("paused");}else if (event.data==YT.PlayerState.PLAYING){playerWrap.classList.remove("ended"); playerWrap.classList.remove("paused");}}; let player; onYouTubeIframeAPIReadyCallbacks.push(function(){player=new YT.Player(playerFrame,{events:{'onStateChange': onPlayerStateChange}});}); playerWrap.addEventListener("click", function(){let playerState=player.getPlayerState(); if (playerState==YT.PlayerState.ENDED){player.seekTo(0);}else if (playerState==YT.PlayerState.PAUSED){player.playVideo();}});}window.onYouTubeIframeAPIReady=function(){for (let callback of onYouTubeIframeAPIReadyCallbacks){callback();}}; window.hideYTActivated=true;});</script>
+    </section>
+
     <!--new 3 services-->
     <section class="grid">
         <div class="col-1-1 sectionservices">
